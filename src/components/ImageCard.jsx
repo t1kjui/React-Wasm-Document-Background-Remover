6346f2e778b83af868ab56c99773f6d832373cec
Dávid Attila <t1kjui@inf.elte.hu>
2023-04-23 10:39:36 +0200

    Added tooltip to the imagecards

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,7 +1,16 @@
+import { dividerClasses } from '@mui/material';
 import React from 'react'
 import './ImageCard.css'
 
 export default function ImageCard({ files, file, removeFile, setFiles, drawImage, testWasm, setLeftDisplayedImage }) {
+
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    if (ref) {
+      console.log(ref);
+    }
+  }, [])
 
   function swapLeft(ev) {
     ev.stopPropagation()
@@ -38,14 +47,31 @@ export default function ImageCard({ files, file, removeFile, setFiles, drawImage
     setLeftDisplayedImage(file);
   }
 
+  function getFileType(mime) {
+    switch (mime) {
+      case "image/bmp":
+        return <div className="imageFormat">BMP</div>
+      case "image/jpeg":
+        return <div className="imageFormatError">JPEG</div>
+      case "image/png":
+        return <div className="imageFormatError">
+                 PNG
+                 <span className="tooltiptext">The background can only be deleted on BMP files!</span>
+               </div>
+      default:
+        console.log("unknown type");
+        return <div id="imageFormatError">UNKOWN</div>
+    }
+  }
+
   return (
     <div style={{ backgroundImage: "url(" + file.URL + ")" }} className='imageCard' onClick={handleDraw}>
       <div className='imageCardTitle'>{file.fileObject.name}</div>
       <button type="button" className={"removeButton"} onClick={handleRemoveFile}>X </button>
       <div id="swapButtons">
         <button type="button" className={"swapButton"} id={"leftSwap"} onClick={swapLeft}>{'<<<'}</button>
+        { getFileType(file.fileObject.type) }
         <button type="button" className={"swapButton"} id={"rightSwap"} onClick={swapRight}>{'>>>'}</button>
-
       </div>
     </div>
   )
